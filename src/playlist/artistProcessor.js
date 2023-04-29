b66869c46eae38b27232b3b317b2a8210b1a28bc
nosavvy33 <bleonp1997@gmail.com>
2023-04-29 13:46:42 -0500

    refactor artists processing

--- src/playlist/artistProcessor.js
+++ src/playlist/artistProcessor.js
@@ -1,8 +1,8 @@
-const { from, lastValueFrom, forkJoin } = require("rxjs");
+const { lastValueFrom, forkJoin } = require("rxjs");
 const { getArtistTopTracks } = require("./spotify");
 const logger = require('../logger/logger');
 
-async function processArtists(artistNames, accessToken) {
+async function getTracksByArtists(artistNames, accessToken) {
     const artistsTopTracks$ = artistNames.map(async (artistName) => {
         logger.debug(`Fetching top tracks for ${artistName}:`);
         const topTracks = await getArtistTopTracks(artistName, accessToken);
@@ -13,13 +13,22 @@ async function processArtists(artistNames, accessToken) {
     return allTopTracks.flat();
 }
 
-
 const getRandomTracks = (tracks, limit = 10) => {
     const shuffledTracks = tracks.sort(() => 0.5 - Math.random());
     return shuffledTracks.slice(0, limit);
 }
 
+async function getTracksForPlaylist(artistNames, accessToken) {
+    const topTracks = await getTracksByArtists(artistNames, accessToken);
+
+    logger.debug(`About to randomize ${topTracks.length} tracks`)
+    logger.debug(`Tracks ${topTracks}`);
+
+    logger.info(`Total hearted tracks from given artists ${topTracks.length}`);
+
+    return getRandomTracks(topTracks, 10 * artistNames.length);
+}
+
 module.exports = {
-    processArtists,
-    getRandomTracks
+    getTracksForPlaylist
 };
--- src/playlist/playlistManager.js
+++ src/playlist/playlistManager.js
@@ -1,33 +1,29 @@
 const { getAccessTokenFromRefreshToken, getUserId, createPlaylist } = require("./spotify");
-const { processArtists, getRandomTracks } = require("./artistProcessor");
+const { getTracksForPlaylist } = require("./artistProcessor");
 const defaultPlaylistName = "Random Tracks from Artists";
 const logger = require('../logger/logger');
 
 async function manageCreatePlaylist(artistNames, playlistName, accessToken, refreshToken) {
+    const finalPlaylistName = playlistName || defaultPlaylistNameParser(artistNames);
+
+    logger.info(`Starting to process playlist ${finalPlaylistName}`);
+
+    const updatedAccessToken = await getAccessTokenFromRefreshToken(refreshToken);
+    const userId = await getUserId(updatedAccessToken);
+
+    var randomTracks = await getTracksForPlaylist(artistNames, updatedAccessToken);
+    logger.debug(`Creating playlist with uris ${randomTracks}`);
+    logger.info(`Creating playlist with ${randomTracks.length} tracks`);
+
+    await createPlaylist(userId, updatedAccessToken, finalPlaylistName, randomTracks.map((track) => track.uri));
+}
+
+function defaultPlaylistNameParser(artistNames) {
     const artists = artistNames.map(artist => artist.trim());
 
     // If the user didn't provide a playlist name, create a default name
     const artistString = artists.join(', ');
-    const defaultPlaylistName = `MashUp: ${artistString}`;
-    const finalPlaylistName = playlistName || defaultPlaylistName;
-
-    logger.info(`Starting to process playlist for ${artistString}`);
-
-    const updatedAccessToken = await getAccessTokenFromRefreshToken(refreshToken);
-
-    const topTracks = await processArtists(artists, updatedAccessToken);
-
-    logger.debug(`About to randomize ${topTracks.length} tracks`)
-    logger.debug(`Tracks ${topTracks}`);
-
-    logger.info(`Total hearted tracks from given artists ${topTracks.length}, creating playlist...`);
-
-    const randomTracks = getRandomTracks(topTracks, 10 * artists.length);
-
-    logger.debug(`Creating playlist with uris ${randomTracks}`);
-
-    const userId = await getUserId(updatedAccessToken);
-    await createPlaylist(userId, accessToken, finalPlaylistName, randomTracks.map((track) => track.uri));
+    return `MashUp: ${artistString}`;
 }
 
 module.exports = { manageCreatePlaylist }
